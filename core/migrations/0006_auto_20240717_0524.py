e949d2ba37bfe02d2ebb45299fe085579037a83d
NwaforChukwuebuka <nwafor.chukwuebuka.samuel@gmail.com>
2024-07-17 17:27:51 +0100

    swapped default user with customUser

---
+++ core/migrations/0006_auto_20240717_0524.py
@@ -0,0 +1,27 @@
+# core/migrations/0006_auto_20240717_0524.py
+
+import os
+from django.db import migrations
+from django.contrib.auth.hashers import make_password
+
+def create_superuser(apps, schema_editor):
+    # Get the custom user model
+    CustomUser = apps.get_model('users', 'CustomUser')
+    if not CustomUser.objects.filter(username='admin').exists():
+        CustomUser.objects.create(
+            username='admin',
+            email=os.getenv('DJANGO_SUPERUSER_EMAIL', 'admin@example.com'),
+            password=make_password(os.getenv('DJANGO_SUPERUSER_PASSWORD', 'adminpassword')),
+            is_staff=True,
+            is_superuser=True
+        )
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0005_auto_20240714_0301'),  # Adjust to your latest migration dependency
+    ]
+
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
